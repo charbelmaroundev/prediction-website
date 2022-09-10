11edafda3a6515d3b2127dbe58f95b01d3ec4264
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-10 22:22:51 +0300

    add-fetch-img

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const ageEl = document.getElementById('age');
 const genderEl = document.getElementById('gender')
 const nationality1El = document.getElementById('nationality1')
 const nationality2El = document.getElementById('nationality2')
+
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => imgEl.src = data.message);
