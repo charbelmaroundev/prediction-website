ef5fbb7ae883bb1e3252f9c27cd81f0cf6df2293
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-10 22:21:42 +0300

    add-js-element

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("TEST");
+const inputEl = document.getElementById("input");
+const btnEl = document.querySelector(".btn")
+const imgEl = document.querySelector('.dog-img')
+const ageEl = document.getElementById('age');
+const genderEl = document.getElementById('gender')
+const nationality1El = document.getElementById('nationality1')
+const nationality2El = document.getElementById('nationality2')
