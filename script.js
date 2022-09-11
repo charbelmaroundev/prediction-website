c813c65fc6db0d667547cace1537579d3a15a87a
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-11 09:24:05 +0300

    add-comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//  ELEMENTS
 const inputEl = document.getElementById("input");
 const btnEl = document.querySelector(".btn")
 const imgEl = document.querySelector('.dog-img')
@@ -7,11 +8,12 @@ const nationality1El = document.getElementById('nationality1')
 const nationality2El = document.getElementById('nationality2')
 const text = document.querySelector(".none")
 
+// FETCH FOR THE IMAGE
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
     .then(data => imgEl.src = data.message);
 
-
+// ADD EVENT LISTENER
 btnEl.addEventListener('click', (e) => {
     e.preventDefault()
 
@@ -19,14 +21,17 @@ btnEl.addEventListener('click', (e) => {
         text.classList.remove("none")
     } else {
         text.classList.add("none")
+        // FETCH FOR GENDER
         fetch(`https://api.genderize.io/?name=${inputEl.value}`)
             .then(response => response.json())
             .then(data => genderEl.innerHTML = data.gender);
 
+        // FETCH FOR AGE  
         fetch(`https://api.agify.io/?name=${inputEl.value}`)
             .then(response => response.json())
             .then(data => ageEl.innerHTML = data.age);
 
+        // FETCH FOR NATIONALITY
         fetch(`https://api.nationalize.io/?name=${inputEl.value}`)
             .then(response => response.json())
             .then(data => nationality1El.innerHTML = data.country['0'].country_id);
