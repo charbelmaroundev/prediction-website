821a0c6dee4e4bd43f5f3c007ef6ae6d977feacc
CharbelMarounDev <charbelmaroun.dev@gmail.com>
2022-09-11 08:30:54 +0300

    add-some-css

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const ageEl = document.getElementById('age');
 const genderEl = document.getElementById('gender')
 const nationality1El = document.getElementById('nationality1')
 const nationality2El = document.getElementById('nationality2')
+const text = document.querySelector(".none")
 
 fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
@@ -14,19 +15,24 @@ fetch('https://dog.ceo/api/breeds/image/random')
 btnEl.addEventListener('click', (e) => {
     e.preventDefault()
 
-    fetch(`https://api.genderize.io/?name=${inputEl.value}`)
-        .then(response => response.json())
-        .then(data => genderEl.innerHTML = data.gender);
+    if (inputEl.value == "") {
+        text.classList.remove("none")
+    } else {
+        text.classList.add("none")
+        fetch(`https://api.genderize.io/?name=${inputEl.value}`)
+            .then(response => response.json())
+            .then(data => genderEl.innerHTML = data.gender);
 
-    fetch(`https://api.agify.io/?name=${inputEl.value}`)
-        .then(response => response.json())
-        .then(data => ageEl.innerHTML = data.age);
+        fetch(`https://api.agify.io/?name=${inputEl.value}`)
+            .then(response => response.json())
+            .then(data => ageEl.innerHTML = data.age);
 
-    fetch(`https://api.nationalize.io/?name=${inputEl.value}`)
-        .then(response => response.json())
-        .then(data => nationality1El.innerHTML = data.country['0'].country_id);
+        fetch(`https://api.nationalize.io/?name=${inputEl.value}`)
+            .then(response => response.json())
+            .then(data => nationality1El.innerHTML = data.country['0'].country_id);
 
-    fetch(`https://api.nationalize.io/?name=${inputEl.value}`)
-        .then(response => response.json())
-        .then(data => nationality2El.innerHTML = data.country['1'].country_id);
+        fetch(`https://api.nationalize.io/?name=${inputEl.value}`)
+            .then(response => response.json())
+            .then(data => nationality2El.innerHTML = data.country['1'].country_id);
+    }
 })
